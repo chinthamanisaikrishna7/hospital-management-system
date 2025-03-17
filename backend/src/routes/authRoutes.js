a41fbfb3196019db5023fbc5740deb65b7216447
CHINTHAMANI SAI KRISHNA <saikrishnaias2004@gmail.com>
2025-03-17 13:04:17 +0530

    Added user login API with JWT authentication

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const bcrypt = require ("bcryptsjs");
 const User = require ("../models/Users");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -15,6 +16,35 @@ router.post("/register", async (req,res) => {
     catch(e){
         res.status(500).json({ error: "Error registering user" });
     }
-})
+});
+
+router.post("/login", async (req,res)=>{
+    const {username,password} = req.body;
+    try{
+        const user = await User.find({username});
+        if(!user){
+            return res.status(400).json({error: "Invalid Credentials"});
+        }
+        else{
+            const isMatched = await bcrypt.compare(password,user.password);
+            if(!isMatched){
+                return res.status(400).json({error: "Invalid Credentials"});
+            }
+            else{
+                const token = jwt.sign(
+                    { userId: user._id, role: user.role },
+                    "yourSecretKey",
+                    { expiresIn: "1h" }
+                  );
+              
+                  res.json({ token, role: user.role });
+            }
+        }
+        
+    }
+    catch (e){
+        res.status(500).json({ error: "Error logging in" });
+    }
+});
 
 module.exports=router;
